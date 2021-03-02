b6fe0980e5ee6a5396979c7fbfc232cdee79ea6d
Ahmed Baligh <ahmedm11@hotmail.com>
2021-03-02 19:17:17 +0200

    feat: Add data for initial decks

---
+++ utils/data.js
@@ -0,0 +1,110 @@
+export const initialDecks = {
+  1: {
+    title: 'Astronomy',
+    cards: [
+      {
+        question: 'What is Star?',
+        answer:
+          'A star is an astronomical object consisting of a luminous spheroid of plasma held together by its own gravity. The nearest star to Earth is the Sun.'
+      },
+      {
+        question: 'How old is the universe?',
+        answer: '13.8 billion (13800000000) years old!'
+      },
+      {
+        question: 'What is a Supernova?',
+        answer:
+          'A supernova is the biggest explosion that humans have ever seen. Each blast is the extremely bright, super-powerful explosion of a star.'
+      },
+      {
+        question: 'How does a black hole form?',
+        answer:
+          "A black hole can be formed by the death of a massive star. When such a star has exhausted the internal thermonuclear fuels in its core at the end of its life, the core becomes unstable and gravitationally collapses inward upon itself, and the star's outer layers are blown away."
+      }
+    ]
+  },
+  2: {
+    title: 'Physics',
+    cards: [
+      {
+        question: 'What is Dark Matter?',
+        answer:
+          "Dark matter is a form of matter thought to account for approximately 85% of the matter in the universe. It's called dark because it does not appear to interact with the electromagnetic field, which means it does not absorb, reflect or emit electromagnetic radiation, and is therefore difficult to detect."
+      },
+      {
+        question: 'How many dimensions does String Theory require?',
+        answer: '10 dimensions!'
+      },
+      {
+        question: 'Is light a wave or a particle?',
+        answer: 'Both! Electromagnetic radiation is known for its dual nature.'
+      },
+      {
+        question:
+          'Without a containing medium, if a feather and a bar of steel are in Free Fall, which of them hits the ground first?',
+        answer:
+          'They both hit the ground at the same time! Gravity is independant of mass.'
+      }
+    ]
+  },
+  3: {
+    title: 'Math',
+    cards: [
+      {
+        question: `Estimate this answer (exact calculation not needed):
+
+          You are planting 468 flowers but want them to go into 12 rows.
+          How many flowers will be in each row?`,
+        answer: 'About 40.'
+      },
+      {
+        question:
+          'A circle is inscribed in a square. An equilateral triangle of side 4√3 units is inscribed in that circle. What is the length of the diagonal of the square?',
+        answer: '8√2 units'
+      },
+      {
+        question: `2004 is divisible by 12 and the sum of its digits is equal to 6.
+
+          Altogether, how many four-digit numbers have these two properties?`,
+        answer: '18.'
+      },
+      {
+        question: `The distance of a particle from its starting point at time t seconds is given by:
+
+          s = 80t - 16 t^2
+
+          What is the velocity of the particle after 2 seconds?`,
+        answer: '16.'
+      }
+    ]
+  },
+  4: {
+    title: 'General Knowledge',
+    cards: [
+      {
+        question: 'Which country has the longest coastline in the world?',
+        answer: 'Canada.'
+      },
+      {
+        question: "What is the world's most populated country?",
+        answer: 'China.'
+      },
+      {
+        question: 'What is the capital of the Philippines?',
+        answer: 'Manila.'
+      },
+      {
+        question: 'The Great Barrier Reef is off the coast of which country?',
+        answer: 'Austrilia.'
+      },
+      {
+        question: "What is the World's Smallest Country?",
+        answer: 'Vatican City.'
+      },
+      {
+        question: "In which country is the world's highest waterfall?",
+        answer: 'Venezuela.'
+      }
+    ]
+  }
+};
